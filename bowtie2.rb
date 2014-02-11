f63d075eec3c993c8e694cc3e49168a1307a3a01
Josh Herr <joshua.r.herr@gmail.com>
2014-02-11 10:23:08 -0500

    Updated bowtie2 to version 2.2.0
    bowtie-align removed from bin-install; function merged with new version of Bowtie2

--- bowtie2.rb
+++ bowtie2.rb
@@ -1,17 +1,16 @@
-
-require 'formula'
+require "formula"
 
 class Bowtie2 < Formula
-  homepage 'http://bowtie-bio.sourceforge.net/bowtie2/index.shtml'
-  url 'http://downloads.sourceforge.net/project/bowtie-bio/bowtie2/2.2.0/bowtie2-2.2.0-macos-x86_64.zip'
-  sha256 '5f4dd63d1517f5006f8bf8dfd5494b34fff564c3c6426613e2fde1665769d640'
+  homepage "http://bowtie-bio.sourceforge.net/bowtie2/index.shtml"
+  url "http://sourceforge.net/projects/bowtie-bio/files/bowtie2/2.2.0/bowtie2-2.2.0-source.zip"
+  sha1 "2a96ba39a61e48abe014ca32258e5dda2263c0f4"
 
   def install
     system "make"
-    bin.install %W(bowtie2 bowtie2-align bowtie2-build bowtie2-inspect)
+    bin.install %W(bowtie2 bowtie2-build bowtie2-inspect)
   end
 
-  def test
-    system "bowtie2", "--version"
+  test do
+    system "false"
   end
 end
